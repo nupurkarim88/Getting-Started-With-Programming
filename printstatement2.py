bde477e410131732814b44c28144dc9fca903e7e
nupurkarim88 <nupurkarim88@gmail.com>
2025-03-09 15:33:39 +0600

    Getting Started With Programming

---
+++ printstatement2.py
@@ -0,0 +1,12 @@
+# Creating different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "Codingal"
+print(codingal)
+
+# UIser Input
+name = input("Enter your name: ")
+print("\nHello", name, "\nwelcome to Codingal")
